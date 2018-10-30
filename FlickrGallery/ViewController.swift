72c875d71e4bc977a17289156f644601706c7d6d
Christian Husunu <christian.husunu@gmail.com>
2018-10-30 20:47:43 +0000

    Initial Commit

---
+++ FlickrGallery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlickrGallery
+//
+//  Created by Christian on 30/10/2018.
+//  Copyright © 2018 Christian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
